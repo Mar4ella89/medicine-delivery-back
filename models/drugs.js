0166627b86baf75ed4caa0f804f3d0ed986747bf
Marat Baikin <3371335@gmail.com>
2024-03-10 23:15:55 +0200

    Update drugs.js

--- models/drugs.js
+++ models/drugs.js
@@ -1,70 +1,70 @@
-// const { Schema, model } = require("mongoose");
-// const Joi = require("joi");
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
 
-// const { hadleMongooseError } = require("../helpers");
+const { hadleMongooseError } = require("../helpers");
 
-// const drugsSchema = new Schema(
-//   {
-//     name: {
-//       type: String,
-//       required: [true, "Set name for contact"],
-//     },
-//     email: {
-//       type: String,
-//       required: [true, "Set email for contact"],
-//     },
-//     phone: {
-//       type: String,
-//       required: [true, "Set phone for contact"],
-//     },
-//     favorite: {
-//       type: Boolean,
-//       default: false,
-//     },
-//     owner: {
-//       type: Schema.Types.ObjectId,
-//       ref: "user",
-//       required: true,
-//     },
-//   },
-//   { versionKey: false, timestamps: true }
-// );
+const drugsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    // email: {
+    //   type: String,
+    //   required: [true, "Set email for contact"],
+    // },
+    // phone: {
+    //   type: String,
+    //   required: [true, "Set phone for contact"],
+    // },
+    // favorite: {
+    //   type: Boolean,
+    //   default: false,
+    // },
+    // owner: {
+    //   type: Schema.Types.ObjectId,
+    //   ref: "user",
+    //   required: true,
+    // },
+  },
+  { versionKey: false, timestamps: true }
+);
 
-// contactSchema.post("save", hadleMongooseError);
+drugsSchema.post("save", hadleMongooseError);
 
-// const addSchema = Joi.object({
-//   name: Joi.string().min(3).max(30).required().messages({
-//     "any.required": `"name" is required`,
-//     "string.empty": `"name" cannot be empty`,
-//     "string.base": `"name" must be string`,
-//   }),
-//   email: Joi.string()
-//     .email({
-//       minDomainSegments: 2,
-//     })
-//     .required()
-//     .messages({
-//       "any.required": `"email" is required`,
-//       "string.empty": `"email" cannot be empty`,
-//     }),
-//   phone: Joi.string().required().messages({
-//     "any.required": `"phone" is required`,
-//     "string.empty": `"phone" cannot be empty`,
-//   }),
-//   favorite: Joi.boolean(),
-// });
+const addSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required().messages({
+    "any.required": `"name" is required`,
+    "string.empty": `"name" cannot be empty`,
+    "string.base": `"name" must be string`,
+  }),
+  // email: Joi.string()
+  //   .email({
+  //     minDomainSegments: 2,
+  //   })
+  //   .required()
+  //   .messages({
+  //     "any.required": `"email" is required`,
+  //     "string.empty": `"email" cannot be empty`,
+  //   }),
+  // phone: Joi.string().required().messages({
+  //   "any.required": `"phone" is required`,
+  //   "string.empty": `"phone" cannot be empty`,
+  // }),
+  favorite: Joi.boolean(),
+});
 
-// const updateFavoriteSchema = Joi.object({
-//   favorite: Joi.boolean().required().messages({
-//     "any.required": `"favorite status" is required`,
-//   }),
-// });
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required().messages({
+    "any.required": `"favorite status" is required`,
+  }),
+});
 
-// const schemas = {
-//   addSchema,
-//   updateFavoriteSchema,
-// };
+const schemas = {
+  addSchema,
+  updateFavoriteSchema,
+};
 
-// const Drug = model("drug", drugSchema);
+const Drugs = model("drugs", drugsSchema);
 
-// module.exports = { Contact, schemas };
+module.exports = { Drugs, schemas };
