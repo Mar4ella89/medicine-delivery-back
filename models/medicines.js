bf1843942255ad508ba13592e5ddd1dcdb8319a4
Marat Baikin <3371335@gmail.com>
2024-03-10 22:40:51 +0200

    Update medicines.js

--- models/medicines.js
+++ models/medicines.js
@@ -9,43 +9,43 @@ const medicinesSchema = new Schema(
       type: String,
       required: [true, "Set name for medicines"],
     },
-    // favorite: {
-    //   type: Boolean,
-    //   default: false,
-    // },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
   { versionKey: false, timestamps: true }
 );
 
 medicinesSchema.post("save", hadleMongooseError);
 
-// const addSchema = Joi.object({
-//   name: Joi.string().min(3).max(30).required().messages({
-//     "any.required": `"name" is required`,
-//     "string.empty": `"name" cannot be empty`,
-//     "string.base": `"name" must be string`,
-//   }),
-//   email: Joi.string()
-//     .email({
-//       minDomainSegments: 2,
-//     })
-//     .required()
-//     .messages({
-//       "any.required": `"email" is required`,
-//       "string.empty": `"email" cannot be empty`,
-//     }),
-//   phone: Joi.string().required().messages({
-//     "any.required": `"phone" is required`,
-//     "string.empty": `"phone" cannot be empty`,
-//   }),
-//   favorite: Joi.boolean(),
-// });
+const addSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required().messages({
+    "any.required": `"name" is required`,
+    "string.empty": `"name" cannot be empty`,
+    "string.base": `"name" must be string`,
+  }),
+  // email: Joi.string()
+  //   .email({
+  //     minDomainSegments: 2,
+  //   })
+  //   .required()
+  //   .messages({
+  //     "any.required": `"email" is required`,
+  //     "string.empty": `"email" cannot be empty`,
+  //   }),
+  // phone: Joi.string().required().messages({
+  //   "any.required": `"phone" is required`,
+  //   "string.empty": `"phone" cannot be empty`,
+  // }),
+  favorite: Joi.boolean(),
+});
 
-// const updateFavoriteSchema = Joi.object({
-//   favorite: Joi.boolean().required().messages({
-//     "any.required": `"favorite status" is required`,
-//   }),
-// });
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required().messages({
+    "any.required": `"favorite status" is required`,
+  }),
+});
 
 const schemas = {
   addSchema,
