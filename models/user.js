dd4cc8741247d648f2a759adb2523943a668e500
Marat Baikin <3371335@gmail.com>
2024-03-10 22:50:55 +0200

    commented user.js

--- models/user.js
+++ models/user.js
@@ -1,98 +1,98 @@
-const { Schema, model } = require("mongoose");
-const Joi = require("joi");
+// const { Schema, model } = require("mongoose");
+// const Joi = require("joi");
 
-const { hadleMongooseError } = require("../helpers");
+// const { hadleMongooseError } = require("../helpers");
 
-const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-const subscriptionList = ["starter", "pro", "business"];
+// const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+// const subscriptionList = ["starter", "pro", "business"];
 
-const userSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: [true, "Name is required"],
-    },
-    email: {
-      type: String,
-      required: [true, "Email is required"],
-      match: emailRegexp,
-      unique: true,
-    },
-    password: {
-      type: String,
-      minlength: 6,
-      required: [true, "Password is required"],
-    },
+// const userSchema = new Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: [true, "Name is required"],
+//     },
+//     email: {
+//       type: String,
+//       required: [true, "Email is required"],
+//       match: emailRegexp,
+//       unique: true,
+//     },
+//     password: {
+//       type: String,
+//       minlength: 6,
+//       required: [true, "Password is required"],
+//     },
 
-    subscription: {
-      type: String,
-      enum: subscriptionList,
-      default: "starter",
-    },
-    token: {
-      type: String,
-      default: null,
-    },
-    avatarURL: {
-      type: String,
-      required: true,
-    },
-    verify: {
-      type: Boolean,
-      default: false,
-    },
-    verificationToken: {
-      type: String,
-      default: "",
-    },
-  },
-  { versionKey: false, timestamps: true }
-);
+//     subscription: {
+//       type: String,
+//       enum: subscriptionList,
+//       default: "starter",
+//     },
+//     token: {
+//       type: String,
+//       default: null,
+//     },
+//     avatarURL: {
+//       type: String,
+//       required: true,
+//     },
+//     verify: {
+//       type: Boolean,
+//       default: false,
+//     },
+//     verificationToken: {
+//       type: String,
+//       default: "",
+//     },
+//   },
+//   { versionKey: false, timestamps: true }
+// );
 
-userSchema.post("save", hadleMongooseError);
+// userSchema.post("save", hadleMongooseError);
 
-const registerSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required().messages({
-    "any.required": `"name" is required`,
-    "string.empty": `"name" cannot be empty`,
-    "string.base": `"name" must be string`,
-  }),
-  email: Joi.string().pattern(emailRegexp).required().messages({
-    "any.required": `"email" is required`,
-    "string.empty": `"email" cannot be empty`,
-  }),
-  password: Joi.string().min(6).required(),
-  subscription: Joi.string(),
-});
+// const registerSchema = Joi.object({
+//   name: Joi.string().min(3).max(30).required().messages({
+//     "any.required": `"name" is required`,
+//     "string.empty": `"name" cannot be empty`,
+//     "string.base": `"name" must be string`,
+//   }),
+//   email: Joi.string().pattern(emailRegexp).required().messages({
+//     "any.required": `"email" is required`,
+//     "string.empty": `"email" cannot be empty`,
+//   }),
+//   password: Joi.string().min(6).required(),
+//   subscription: Joi.string(),
+// });
 
-const loginSchema = Joi.object({
-  email: Joi.string().pattern(emailRegexp).required().messages({
-    "any.required": `"email" is required`,
-    "string.empty": `"email" cannot be empty`,
-  }),
-  password: Joi.string().min(6).required(),
-});
+// const loginSchema = Joi.object({
+//   email: Joi.string().pattern(emailRegexp).required().messages({
+//     "any.required": `"email" is required`,
+//     "string.empty": `"email" cannot be empty`,
+//   }),
+//   password: Joi.string().min(6).required(),
+// });
 
-const emailSchema = Joi.object({
-  email: Joi.string().pattern(emailRegexp).required().messages({
-    "any.required": `"email" is required`,
-    "string.empty": `"email" cannot be empty`,
-  }),
-});
+// const emailSchema = Joi.object({
+//   email: Joi.string().pattern(emailRegexp).required().messages({
+//     "any.required": `"email" is required`,
+//     "string.empty": `"email" cannot be empty`,
+//   }),
+// });
 
-const updateSubscriptionSchema = Joi.object({
-  subscription: Joi.string().required().messages({
-    "any.required": `"subscription status" is required`,
-  }),
-});
+// const updateSubscriptionSchema = Joi.object({
+//   subscription: Joi.string().required().messages({
+//     "any.required": `"subscription status" is required`,
+//   }),
+// });
 
-const schemas = {
-  registerSchema,
-  loginSchema,
-  emailSchema,
-  updateSubscriptionSchema,
-};
+// const schemas = {
+//   registerSchema,
+//   loginSchema,
+//   emailSchema,
+//   updateSubscriptionSchema,
+// };
 
-const User = model("user", userSchema);
+// const User = model("user", userSchema);
 
-module.exports = { schemas, User };
+// module.exports = { schemas, User };
---
+++ routes/api/drugs-routes.js
@@ -0,0 +1,39 @@
+const express = require("express");
+
+const ctrl = require("../../controllers/drugs-controllers");
+
+const { isValidId } = require("../../middlewares");
+const { schemas } = require("../../models/drugs");
+
+const router = express.Router();
+
+router.get("/", ctrl.getAlldrugs);
+
+router.get("/:id", isValidId, ctrl.getById);
+
+// router.post(
+//   "/",
+//   authenticate,
+//   validateBody(schemas.addSchema),
+//   ctrl.addContact
+// );
+
+// router.put(
+//   "/:id",
+//   authenticate,
+//   isValidId,
+//   validateBody(schemas.addSchema),
+//   ctrl.updateById
+// );
+
+// router.patch(
+//   "/:id/favorite",
+//   authenticate,
+//   isValidId,
+//   validateBody(schemas.updateFavoriteSchema),
+//   ctrl.updateFavoriteById
+// );
+
+// router.delete("/:id", authenticate, isValidId, ctrl.deleteById);
+
+module.exports = router;
