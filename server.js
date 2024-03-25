befe4b2a988aebcd30245c8a37a5492a556f9b23
Marat Baikin <3371335@gmail.com>
2024-03-25 17:24:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
+require("dotenv").config();
+
 const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   // .connect(
   //   "mongodb+srv://Mar4ella:ktybyuhflcrfz40@cluster0.g1mqafu.mongodb.net/db-drugs?retryWrites=true&w=majority&appName=Cluster0"
   // )
-  .connect(DB_HOST.toString())
+  .connect(DB_HOST)
   .then(() =>
     app.listen(PORT, () => {
       console.log("Database connection successful");
