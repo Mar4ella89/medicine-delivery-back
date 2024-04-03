dc1d0f1e1676153a6662ceb1208d993c1f117321
Marat Baikin <3371335@gmail.com>
2024-04-03 14:56:55 +0300

    Update drugs-controllers.js

--- controllers/drugs-controllers.js
+++ controllers/drugs-controllers.js
@@ -7,13 +7,10 @@ const getAllDrugs = async (req, res) => {
   const { page = 1, limit = 10 } = req.query;
   // const { favorite = true } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Drugs.find();
-  // "-createdAt -updatedAt"
-  // {
-  //   skip,
-  //   limit,
-  // }
-
+  const result = await Drugs.find("-createdAt -updatedAt", {
+    skip,
+    limit,
+  });
   res.json(result);
 };
 
