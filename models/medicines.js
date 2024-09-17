3b22f1dc258ba868bf52accc4cf6c071cbb3be54
Marat Baikin <3371335@gmail.com>
2024-09-17 09:48:11 +0300

    Update medicines.js

--- models/medicines.js
+++ models/medicines.js
@@ -7,32 +7,52 @@ const medicinesSchema = new Schema({
   name: String,
   price: Number,
   imageUrl: String,
-  availablePharmacies: [mongoose.Schema.Types.ObjectId],
+  availablePharmacies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Drugs" }],
 });
 
 medicinesSchema.post("save", hadleMongooseError);
-
 const addSchema = Joi.object({
   name: Joi.string().min(3).max(30).required().messages({
     "any.required": `"name" is required`,
     "string.empty": `"name" cannot be empty`,
     "string.base": `"name" must be string`,
   }),
-  // email: Joi.string()
-  //   .email({
-  //     minDomainSegments: 2,
-  //   })
-  //   .required()
-  //   .messages({
-  //     "any.required": `"email" is required`,
-  //     "string.empty": `"email" cannot be empty`,
-  //   }),
-  // phone: Joi.string().required().messages({
-  //   "any.required": `"phone" is required`,
-  //   "string.empty": `"phone" cannot be empty`,
-  // }),
-  // favorite: Joi.boolean(),
+  price: Joi.number().required().messages({
+    "any.required": `"price" is required`,
+    "number.base": `"price" must be a number`,
+  }),
+  imageUrl: Joi.string().required().messages({
+    "any.required": `"imageUrl" is required`,
+    "string.empty": `"imageUrl" cannot be empty`,
+    "string.base": `"imageUrl" must be string`,
+  }),
+  availablePharmacies: Joi.array().items(Joi.string()).required().messages({
+    "any.required": `"availablePharmacies" is required`,
+    "array.base": `"availablePharmacies" must be an array`,
+  }),
 });
+
+// const addSchema = Joi.object({
+//   name: Joi.string().min(3).max(30).required().messages({
+//     "any.required": `"name" is required`,
+//     "string.empty": `"name" cannot be empty`,
+//     "string.base": `"name" must be string`,
+//   }),
+//   // email: Joi.string()
+//   //   .email({
+//   //     minDomainSegments: 2,
+//   //   })
+//   //   .required()
+//   //   .messages({
+//   //     "any.required": `"email" is required`,
+//   //     "string.empty": `"email" cannot be empty`,
+//   //   }),
+//   // phone: Joi.string().required().messages({
+//   //   "any.required": `"phone" is required`,
+//   //   "string.empty": `"phone" cannot be empty`,
+//   // }),
+//   // favorite: Joi.boolean(),
+// });
 
 // const updateFavoriteSchema = Joi.object({
 //   favorite: Joi.boolean().required().messages({
